c53470508c7fa958eb3c1518bb024e1a3144a297
bsmaciel <contatosbrunomaciel@gmail.com>
2024-11-05 10:29:38 -0300

    Novo diretório com list-app-layout

---
+++ aulas_react_principio/aula3_layout/list-app-layout/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <p>Direitos autorais Bruno Maciel 2024</p>
+        </footer>
+    );
+};
+
+export default Footer;
