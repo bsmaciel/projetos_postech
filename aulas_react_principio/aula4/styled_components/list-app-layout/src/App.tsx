eb0643f9eb01ced809e55fc758da00cc1f5a5137
bsmaciel <contatosbrunomaciel@gmail.com>
2024-11-21 09:36:39 -0300

    Atualizações da Fase 3: React - O princípio, aula 5: Hooks.

---
+++ aulas_react_principio/aula4/styled_components/list-app-layout/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import MainContent from './components/MainContent/MainContent';
+import AddTask from './pages/AddTask/AddTask';
+import TaskList from './pages/TaskList/TaskList';
+import './App.css';
+
+interface Task {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (taskName: string) => {
+    setTasks([...tasks, { id: tasks.length + 1, name: taskName }]);
+  };
+
+  const removeTask = (taskId: number) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  };
+
+  return (
+    <div className="app-container">
+      <Header />
+      <MainContent>
+        <h1>Pendências</h1>
+        <AddTask onAddTask={addTask} />
+        <TaskList tasks={tasks} onRemoveTask={removeTask} />
+      </MainContent>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
