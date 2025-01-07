2277001c4ab41e5c15fd59ee3056a4e98b82449d
bsmaciel <contatosbrunomaciel@gmail.com>
2025-01-07 12:56:02 -0300

    Arquivo fiap-chat movido para o diretório superior na árvore

---
+++ frontend/aulas_next/aula1_estrutura_nextjs/chat_api/src/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+      <main>
+        <p>FIAPCHAT</p>
+        <div>
+          <p>Digite um nome de usuário</p>
+          <input type="text" placeholder=""/>
+          <button>Entrar</button>
+        </div>
+      </main>
+    );
+  }
