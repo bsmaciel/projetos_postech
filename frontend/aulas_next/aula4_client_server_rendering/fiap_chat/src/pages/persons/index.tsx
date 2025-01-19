00bfa3e400db565dcabb99193ad0bcfecc45acb0
bsmaciel <contatosbrunomaciel@gmail.com>
2025-01-19 18:49:33 -0300

    Correções na aula 4, disciplina Next.js, Fase 3.

--- frontend/aulas_next/aula4_client_server_rendering/fiap_chat/src/pages/persons/index.tsx
+++ frontend/aulas_next/aula4_client_server_rendering/fiap_chat/src/pages/persons/index.tsx
@@ -15,17 +15,16 @@ export async function getServerSideProps() {
             return { props: { listaUsuarios: [] } };
         } 
     } catch (error) {
-    console.error('Erro ao buscar dados:', error);
-    return { props: { listaUsuarios: [] } };
+        console.error('Erro ao buscar dados:', error);
+        return { props: { listaUsuarios: [] } };
     }
 
 }
 
-
 export default function Persons({ listaUsuarios }){
     return(
         <main className='flex flex-1 flex-col h-screen w-screen justify-center items-center bg-background-900'>
-            <p className='font-sans text-2xl text-fontColor-900'>listar as pessoas</p>
+            <p className='font-sans text-2xl text-fontColor-900'>Listar os cards dos usuários</p>
             {listaUsuarios.map((item:any) => (
                 <UserCard key={item.id} data={item}/>
             ))}
