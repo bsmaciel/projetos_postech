1e38af77b869fca30400218c034c7b45762edc8e
bsmaciel <contatosbrunomaciel@gmail.com>
2025-02-05 07:41:16 -0300

    Modificações no Tech_Challenge

---
+++ tech_challenge/tech_challenge_3/frontend-blog/src/Screems/createPost/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../../components/Header/index.tsx"
+import Footer from "../../components/Footer/index.tsx"
+
+
+const CreatePost = () => {
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Teste" />
+            </form>
+        </div>
+    )
+}
+
+export default CreatePost;
---
+++ tech_challenge/tech_challenge_3/frontend-blog/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './header.css';
+// import styled from 'styled-components';
+
+const Header: React.FC = () => {
+    return(
+        <header className="header">
+            <h1>HEADER</h1>
+        </header>
+    );
+};
+
+export default Header
