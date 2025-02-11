00dc3fa5b88de463ad2960b73d06b7cf155d89e4
bsmaciel <contatosbrunomaciel@gmail.com>
2025-02-11 14:21:37 -0300

    Tech Challenge 3, trabalhando com syled-components

---
+++ tech_challenge/tech_challenge_3/frontend-blog/src/Screens/post/index.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+// import styled from 'styled-components';
+
+/* const PostContainer = styled.main`
+    background-color: #f1f0ae;
+    color: #000000;
+    text-align: center;
+    padding: 10px 0;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    `;
+ */
+const Post: React.FC = () => {
+    return(
+        // <PostContainer>
+            <>
+                <h1>Post</h1>
+            </>
+        // </PostContainer>
+    )
+}
+
+export default Post;
