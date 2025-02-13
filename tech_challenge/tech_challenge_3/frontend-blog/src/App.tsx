82e49d4d4ffd9fb8ca8ebde51e379dfc5c411d0c
bsmaciel <contatosbrunomaciel@gmail.com>
2025-02-13 10:07:48 -0300

    Ajustes páginas sem cabeçalho e rodapé

--- tech_challenge/tech_challenge_3/frontend-blog/src/App.tsx
+++ tech_challenge/tech_challenge_3/frontend-blog/src/App.tsx
@@ -1,47 +1,47 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import AdminPost from './Screens/adminPost';
-import CreatePost from './Screens/createPost';
-import EditPost from './Screens/editPost';
-import LandingPage from './Screens/landingPage';
-import ListPost from './Screens/listPost';
-import Login from './Screens/login';
-import PasswordReset from './Screens/passwordReset';
-import Post from './Screens/post';
-import SignUp from './Screens/signUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Footer from './components/Footer'
+// import Header from './components/Header'
+import MainContent from './components/MainContent'
+import AdminPost from './Screens/adminPost'
+import CreatePost from './Screens/createPost'
+import EditPost from './Screens/editPost'
+import LandingPage from './Screens/landingPage'
+import ListPost from './Screens/listPost'
+import Login from './Screens/login'
+import PasswordReset from './Screens/passwordReset'
+import SignUp from './Screens/signUp'
+import ViewPost from './Screens/viewPost'
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import '@fortawesome/fontawesome-free/js/all.min.js';
+// import '@fortawesome/fontawesome-free/css/all.min.css'
+// import '@fortawesome/fontawesome-free/js/all.min.js'
 
 
 function App() {
 
   return (
     <Router>
+      {/* <Header /> */}
       <div>
-        <Header />
         <MainContent>
           <Routes>
-            <Route path="/" element={<Login />} />
             <Route path="/adminPost" element={<AdminPost />} />
             <Route path="/createPost" element={<CreatePost />} />
             <Route path="/editPost" element={<EditPost />} />
             <Route path="/landingPage" element={<LandingPage />} />
-            <Route path="/post" element={<Post />} />
-            <Route path="/passwordReset" element={<PasswordReset />} />
             <Route path="/listPost" element={<ListPost />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/passwordReset" element={<PasswordReset />} />
             <Route path="/signUp" element={<SignUp />} />
+            <Route path="/viewpost" element={<ViewPost />} />
           </Routes>
         </MainContent>
-        <Footer />
       </div>
+      {/* <Footer /> */}
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
 
 /*   return (
     <>
