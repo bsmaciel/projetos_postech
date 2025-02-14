7f42d77c4b6449f73e5665cd37c1529e9d115b90
bsmaciel <contatosbrunomaciel@gmail.com>
2025-02-14 11:17:15 -0300

    Tentando controlar a estilização da div id=root.

--- tech_challenge/tech_challenge_3/frontend-blog/src/App.tsx
+++ tech_challenge/tech_challenge_3/frontend-blog/src/App.tsx
@@ -12,35 +12,10 @@ import PasswordReset from './Screens/passwordReset'
 import SignUp from './Screens/signUp'
 import ViewPost from './Screens/viewPost'
 
-<<<<<<< HEAD
+import './App.css'
+
 // import '@fortawesome/fontawesome-free/css/all.min.css'
 // import '@fortawesome/fontawesome-free/js/all.min.js'
-=======
-// import { useState } from 'react'
-// import  { useEffect } from 'react';
-// import  { useReducer } from 'react';
-// import api from './api';
-
-// import { ? } from './types';
-// import './App.css';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-import MainContent from './components/MainContent';
-import Login from './Screens/login';
-import AdminPosts from './Screens/adminPosts';
-import CreatePost from './Screens/createPost';
-import EditPost from './Screens/editPost';
-import LandingPage from './Screens/landingPage';
-import PasswordRescue from './Screens/passwordRescue';
-import Post from './Screens/post';
-import PostList from './Screens/postList';
-import SignUp from './Screens/signUp';
-
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import '@fortawesome/fontawesome-free/js/all.min.js';
->>>>>>> 82e33fd (Desenvolvimento do layout do Tech Challeng, styled-components e display flex)
 
 
 function App() {
@@ -48,11 +23,7 @@ function App() {
   return (
     <Router>
       {/* <Header /> */}
-      <div>
-<<<<<<< HEAD
-=======
-        {/* <Header /> */}
->>>>>>> 82e33fd (Desenvolvimento do layout do Tech Challeng, styled-components e display flex)
+      {/* <div> */}
         <MainContent>
           <Routes>
             <Route path="/adminPost" element={<AdminPost />} />
@@ -66,17 +37,12 @@ function App() {
             <Route path="/viewpost" element={<ViewPost />} />
           </Routes>
         </MainContent>
-<<<<<<< HEAD
-=======
-        {/* <Footer /> */}
->>>>>>> 82e33fd (Desenvolvimento do layout do Tech Challeng, styled-components e display flex)
-      </div>
+      {/* </div> */}
       {/* <Footer /> */}
     </Router>
   )
 }
 
-<<<<<<< HEAD
 export default App
 
 /*   return (
@@ -89,6 +55,3 @@ export default App
     </>
   )
 }   */
-=======
-export default App;
->>>>>>> 82e33fd (Desenvolvimento do layout do Tech Challeng, styled-components e display flex)
--- tech_challenge/tech_challenge_3/frontend-blog/src/Screens/login/index.tsx
+++ tech_challenge/tech_challenge_3/frontend-blog/src/Screens/login/index.tsx
@@ -1,45 +1,29 @@
 import React from "react"
 import './login.css'
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import '@fortawesome/fontawesome-free/js/all.min.js'
+/* import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/js/all.min.js' */
 
 const Login: React.FC = () => {
     return(
-        <>
-            Login
-        </>
+        <div className="container">
+            <h2 className="titulo">LOGIN</h2>
+            <h1 className="subtitulo">Acesse sua conta</h1>
+
+            <form>
+                <input type="text" placeholder="Nome completo" required />
+                <input type="email" placeholder="Email" required />
+                <input type="password" placeholder="Senha" required />
+                
+                <div className="links">
+                    <a href="#">Não tem uma conta?</a>
+                    <a href="#">Esqueceu sua senha?</a>
+                </div>
+
+                <button type="submit" className="btn-entrar">ENTRAR</button>
+            </form>
+        </div>   
     )
 }
 
 export default Login
-
-/* <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div class="container">
-        <h2 class="titulo">LOGIN</h2>
-        <h1 class="subtitulo">Acesse sua conta</h1>
-
-        <form>
-            <input type="text" placeholder="Nome completo" required>
-            <input type="email" placeholder="Email" required>
-            <input type="password" placeholder="Senha" required>
-
-            <div class="links">
-                <a href="#">Não tem uma conta?</a>
-                <a href="#">Esqueceu sua senha?</a>
-            </div>
-
-            <button type="submit" class="btn-entrar">ENTRAR</button>
-        </form>
-    </div>
-</body>
-</html>
- */
