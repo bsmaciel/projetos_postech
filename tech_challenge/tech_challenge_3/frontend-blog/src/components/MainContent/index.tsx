58a1218d77e706a6cd3c9b41c6b8ebf8355c56ab
bsmaciel <contatosbrunomaciel@gmail.com>
2025-02-14 11:17:15 -0300

    Tentando controlar a estilização da div id=root.

--- tech_challenge/tech_challenge_3/frontend-blog/src/components/MainContent/index.tsx
+++ tech_challenge/tech_challenge_3/frontend-blog/src/components/MainContent/index.tsx
@@ -1,33 +1,15 @@
 import React from "react"
 import './mainContent.css'
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import '@fortawesome/fontawesome-free/js/all.min.js'
+// import '@fortawesome/fontawesome-free/css/all.min.css'
+// import '@fortawesome/fontawesome-free/js/all.min.js'
 
-<<<<<<< HEAD
 
 const MainContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return(
         <>
             {children}
         </>
-=======
-const MainContentContainer = styled.main`
-    background: #FFFF00;
-    width: 600px;
-    margin: auto;
-    height: 100vh;
-    text-align: center;
-`;
-
-const MainContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return(
-        <MainContentContainer>
-            <div>
-                {children}
-            </div>
-        </MainContentContainer>
->>>>>>> 82e33fd (Desenvolvimento do layout do Tech Challeng, styled-components e display flex)
     )
 }
 
