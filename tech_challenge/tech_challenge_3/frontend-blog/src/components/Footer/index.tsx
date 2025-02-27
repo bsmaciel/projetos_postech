e37f7c82583ad1ecdd59087b9bbec456cf4a884c
bsmaciel <contatosbrunomaciel@gmail.com>
2025-02-26 21:42:39 -0300

    Estilização da tela adminPost

--- tech_challenge/tech_challenge_3/frontend-blog/src/components/Footer/index.tsx
+++ tech_challenge/tech_challenge_3/frontend-blog/src/components/Footer/index.tsx
@@ -1,16 +1,16 @@
 import React from "react"
 import './footer.css'
 
-// import '@fortawesome/fontawesome-free/css/all.min.css'
-// import '@fortawesome/fontawesome-free/js/all.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/js/all.min.js'
 
 const Footer: React.FC = () => {
     return(
         <>
-        <footer>
-            <i className="fas fa-arrow-left back-icon"></i>
-            <span>FIAP</span>
-        </footer>
+            <footer>
+                <i className="fas fa-arrow-left back-icon"></i>
+                <span>FIAP</span>
+            </footer>
         </>
     )
 }
