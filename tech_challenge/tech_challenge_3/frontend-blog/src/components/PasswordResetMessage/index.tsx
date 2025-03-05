8e8e46f75b570ab171c25d08f4179a22b5729690
bsmaciel <contatosbrunomaciel@gmail.com>
2025-03-05 20:34:28 -0300

    Construção das páginas PassWordMessage e AdminPostsAlert

--- tech_challenge/tech_challenge_3/frontend-blog/src/components/PasswordResetMessage/index.tsx
+++ tech_challenge/tech_challenge_3/frontend-blog/src/components/PasswordResetMessage/index.tsx
@@ -1,2 +1,19 @@
-// import React from "react";
-// import './passwordResetMessage.css'
+import React from "react";
+import './passwordResetMessage.css'
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/js/all.min.js'
+
+const PasswordResetMessage: React.FC = () => {
+    return(
+        <div className="container-passwordResetMessage">
+            <div className="box">
+                <h2 className="titulo-passwordResetMessage">MENSAGEM</h2>
+                <p className="feedback-text">Sua senha foi enviada<br/>Confira seu e-mail</p>
+                <button type="submit" className="btn-ok">OK</button>
+            </div>
+        </div>
+    )
+}
+
+export default PasswordResetMessage
